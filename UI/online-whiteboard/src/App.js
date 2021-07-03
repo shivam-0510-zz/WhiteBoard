e2200f1da8d6eae24057226cef9f473ce80d3213
Shivam Gupta <shivam.gupta5102@gmail.com>
2021-07-03 16:24:53 +0530

    Add files via upload

---
+++ UI/online-whiteboard/src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+import Container from './Components/Container/Container';
+
+function App() {
+  return (
+    <Container/>
+  );
+}
+
+export default App;
